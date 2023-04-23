a5ba6c62d7bc5c5eac04c6ab6e795b50ea9e1e0f
Abdou Bhm <businessbbhm@gmail.com>
2023-04-23 23:20:50 +0100

    commit for pagination thymeleaf html ..

--- patient/src/main/java/com/babahamou/patient/web/Controller.java
+++ patient/src/main/java/com/babahamou/patient/web/Controller.java
@@ -3,8 +3,11 @@ package com.babahamou.patient.web;
 import com.babahamou.patient.entities.Patient;
 import com.babahamou.patient.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -18,9 +21,12 @@ public class Controller {
         return "test";
     }
     @GetMapping(path = "/patients")
-    public  String list(Model model){
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("patients", patients);
+    public  String list(Model model, @RequestParam(name="page", defaultValue = "0") int page,@RequestParam(name="size", defaultValue = "6") int size){
+        Page<Patient> pagepatients = patientRepository.findAll(PageRequest.of(page,size));
+        model.addAttribute("patients", pagepatients);
+        model.addAttribute("pages", new int[pagepatients.getTotalPages()]);
+        model.addAttribute("currentPage", page);
+
 
         return "patient";
     }
